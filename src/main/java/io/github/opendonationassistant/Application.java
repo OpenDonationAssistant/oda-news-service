e97cb3555b0d48726203768fa68f06c24242ff67
stCarolas <stcarolas@gmail.com>
2024-09-23 15:02:32 +0300

    remove some unesseccary logging

--- src/main/java/io/github/opendonationassistant/Application.java
+++ src/main/java/io/github/opendonationassistant/Application.java
@@ -34,7 +34,6 @@ public class Application {
   }
 
   public static void main(String[] args) {
-    Micronaut.run(Application.class, args);
+    Micronaut.build(args).banner(false).classes(Application.class).start();
   }
 }
-
